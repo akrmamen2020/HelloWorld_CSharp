7beeb25e20a60814411b9d9be9a466698ee35e99
al Mohamady <al.Mohamady47@gmail.com>
2020-01-04 15:02:28 +0200

    String type

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,20 +4,10 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello world");
 
-        string stringVar = "Hello World!!";
-        int intVar = 100;
-        float floatVar = 10.2f;
-        char charVar = 'A';
-        bool boolVar = true;
+        string stringVar = @"C:\newFolder\fasd";
 
-        Console.WriteLine("Max int Value {0}", int.MaxValue);
-        Console.WriteLine("Min int Value {0}", int.MinValue);
-
-        int d = 2014512454;
-
-        Console.WriteLine("The double number is {0}", d);
+        Console.WriteLine(stringVar);
 
         Console.ReadKey(true);
     }
