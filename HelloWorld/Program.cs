55c74ad6f0fb889e8bb9639760cc9e678b025800
al Mohamady <al.Mohamady47@gmail.com>
2020-01-04 15:38:34 +0200

    Common Operators

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,9 +5,24 @@ class Program
     static void Main()
     {
 
-        string stringVar = @"C:\newFolder\fasd";
+        string str = "Ahmed";
+        int i = 10;
+        bool b = true;
 
-        Console.WriteLine(stringVar);
+        i = 20;
+
+
+        int num1 = 11;
+        int num2 = 2;
+        int num3 = 10;
+
+        int result = num1 % num2;
+
+        bool isTrue = ((num1 != num2) && (num1 == num3)) ;
+
+        bool isNumber10 = num1 <= 10 ? true : false;
+
+        Console.WriteLine("the result = " + isNumber10);
 
         Console.ReadKey(true);
     }
