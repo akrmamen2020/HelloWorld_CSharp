00b2012d5342e09cfb45d1e052ab89459382f3b1
al Mohamady <al.Mohamady47@gmail.com>
2020-01-04 14:06:53 +0200

    init commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello world");
+
+        Console.WriteLine("Enter Your First Name :");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Last Name :");
+        string lastName = Console.ReadLine();
+
+
+        Console.WriteLine("Welcome Back " + firstName + " " + lastName);
+        Console.WriteLine("Your Acount is Active Mr.{0} {1}", firstName, lastName);
+
+
+
+
+        Console.ReadKey(true);
+    }
+}
+
