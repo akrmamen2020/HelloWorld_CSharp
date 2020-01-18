904926ae005d855bba5f14b9c9cb51c14f5fa863
al Mohamady <al.Mohamady47@gmail.com>
2020-01-18 16:42:15 +0200

    Method parameters

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,40 +5,47 @@ class Program
     static void Main()
     {
 
-        //Program p = new Program();
-        //Program.EvenNumbers(40);
-        //Console.WriteLine(Program.addNumbers(20, 15));
 
-        Program.EvenNumbers(9);
+        //int i = 0;
 
+        //method(ref i);
+
+        // int div = 0;
+        // int mult = 0;
+
+        //Console.WriteLine(calculate(20, 10, out div, out mult));
+
+        // Console.WriteLine(div);
+        // Console.WriteLine(mult);
+        //Console.WriteLine(i);
+
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        arrMethod("ahmed",1, 2, 3, 4, 5, 6);
 
         Console.ReadKey(true);
     }
 
-    public static int addNumbers(int num1,int num2)
+    public static void arrMethod(string a, params int[] args)
     {
-        int result = num1 + num2;
-        return result;
+        foreach (int i in args)
+        {
+            Console.WriteLine(i);
+        }
     }
 
-    public static void EvenNumbers(int target)
+
+    public static int calculate(int num1, int num2, out int div, out int mult)
     {
-        int Start = 0;
-
-        if (target <= 10)
-        {
-            Console.WriteLine("Number is < 10");
-            return;
-        }
-
-        while (Start <= target)
-        {
-            Console.WriteLine("the number is {0} ", Start);
-            Start += 2;
-        }
-
+        div = num1 / num2;
+        mult = num1 * num2;
+        return num1 + num2;
     }
 
+    public static void method(ref int j)
+    {
+        j = 200;
+    }
 }
 
 
