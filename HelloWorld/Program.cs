08eb9c38afca099a5cf5603ed9b2ccfaebf5e34d
al Mohamady <al.Mohamady47@gmail.com>
2020-01-18 14:06:50 +0200

    while loop in c#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,46 +5,26 @@ class Program
     static void Main()
     {
 
-        int total = 0;
 
+    
+        int Start = 0;
+        string rst = "";
 
-        Start:
-
-        Console.WriteLine("select your sandwich : 1- Large , 2- x-Large, 3 xx-Large");
-        int item = int.Parse(Console.ReadLine());
-
-
-        switch (item)
+        do
         {
-            case 1:
-                total += 10;
-                break;
-            case 2:
-                total += 20;
-                break;
-            case 3:
-                total += 30;
-                goto case 1;
-            default:
-                Console.WriteLine("Your choice {0} is invalid");
-                break;
-        }
+            Console.WriteLine("Please Set your target");
+            int theTarget = int.Parse(Console.ReadLine());
+            while (Start <= theTarget)
+            {
+                Console.WriteLine(" your number is {0} ", Start);
+                Start++;
+            }
+            Console.WriteLine("Do you want to play again ( y-Yes, n- No)");
+            rst = Console.ReadLine();
+            Start = 0;
+        } while (rst == "y");
 
-        Console.WriteLine("Your choice is : {0} $", item);
-
-        Console.WriteLine("Do you want another sandwich ? y-Yes , n-No");
-        string rst = Console.ReadLine();
-
-        if (rst == "y")
-        {
-            goto Start;
-        }
-        else
-        {
-
-        }
-
-        Console.WriteLine("Total Check is : {0} $", total);
+        Console.WriteLine("Game over");
 
         Console.ReadKey(true);
     }
