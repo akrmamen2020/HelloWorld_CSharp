038583e7bcaca7d5906bbc8c502be26de150a60b
al Mohamady <al.Mohamady47@gmail.com>
2020-01-21 00:59:09 +0200

    Namespaces

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,51 +1,59 @@
 ﻿using System;
+using PATA = ProjectA.TeamA;
+using PBTB = ProjectA.TeamB;
 
 class Program
 {
     static void Main()
     {
 
-
-        //int i = 0;
-
-        //method(ref i);
-
-        // int div = 0;
-        // int mult = 0;
-
-        //Console.WriteLine(calculate(20, 10, out div, out mult));
-
-        // Console.WriteLine(div);
-        // Console.WriteLine(mult);
-        //Console.WriteLine(i);
-
-        int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
-
-        arrMethod("ahmed",1, 2, 3, 4, 5, 6);
+        PATA.ClassA.Print();
+        PBTB.ClassB.Print();
 
         Console.ReadKey(true);
-    }
-
-    public static void arrMethod(string a, params int[] args)
-    {
-        foreach (int i in args)
-        {
-            Console.WriteLine(i);
-        }
-    }
-
-
-    public static int calculate(int num1, int num2, out int div, out int mult)
-    {
-        div = num1 / num2;
-        mult = num1 * num2;
-        return num1 + num2;
-    }
-
-    public static void method(ref int j)
-    {
-        j = 200;
     }
 }
 
 
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public  static void Print()
+            {
+                Console.WriteLine("Hello Class A");
+       
+            }
+        }
+    }
+
+
+}
+
+namespace ProjectA
+{
+    namespace TeamB
+    {
+
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Hello Class A");
+
+            }
+        }
+
+        class ClassB
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Hello Class B");
+
+            }
+        }
+
+    }
+}
