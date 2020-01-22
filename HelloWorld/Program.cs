fbb207eb0c4eff72c5b84e1de73eed5310e2d927
al Mohamady <al.Mohamady47@gmail.com>
2020-01-22 17:49:23 +0200

    Method overloading in c#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,44 +1,30 @@
 ﻿using System;
 
 
-public class Father
-{
-    public virtual void Print()
-    {
-        Console.WriteLine("Hello Father. :)");
-    }
-
-    public void myName()
-    {
-        Console.WriteLine("Father Name. :)");
-    }
-}
-
-public class Son : Father
-{
-    public override void Print()
-    {
-        Console.WriteLine("Hello Son. :)");
-    }
-
-    public new void myName()
-    {
-        Console.WriteLine("Son Name. :)");
-    }
-}
-
 class Program
 {
     static void Main()
     {
 
-        Father father = new Son();
-        father.Print();
-        Console.WriteLine("===================================");
-        father.myName();
+        add(5, 6, 1,1,1,1,1);
+
+        int[] arr = new int[4];  
+
+        add(5, 0, false, arr);
 
         Console.ReadKey(false);
- 
+    }
+
+    private static int add(int n2, int n1, params int[] arr)
+    {
+        Console.WriteLine("Sum = {0} ", n1 + n2);
+        return n1 + n2; 
+    }
+
+    public static void add(int n2, int n1, int[] arr)
+    {
+        n1 = 20;
+        Console.WriteLine("Sum = {0} ", n1 + n2);
     }
 }
 
