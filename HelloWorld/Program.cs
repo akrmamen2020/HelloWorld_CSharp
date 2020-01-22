513dc70f08498b77b5de2b87d327e60917a995b2
al Mohamady <al.Mohamady47@gmail.com>
2020-01-22 16:23:47 +0200

    Method hiding in c#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,58 +7,42 @@ public class Employee
     public string LastName;
     public string Address;
 
-
-    public Employee()
-    {
-        Console.WriteLine("Emp Cons");
-    }
-
-    public Employee(string key)
-    {
-        Console.WriteLine("Emp Cons : key is {0} " , key);
-    }
-
     public void PrintEmpInfo()
     {
         Console.WriteLine(FirstName + "  " + LastName);
     }
 }
 
-
 public class FullTimeEmployee : Employee 
 {
 
-    public FullTimeEmployee() : base("Ahmed")
-    {
-        Console.WriteLine("Full Emp Cons");
-    }
-
-    public float theSalary; 
 }
 
-public class PartTimeEmployee : FullTimeEmployee
+public class PartTimeEmployee : Employee
 {
-    public float thePartSalary;
-
+    public new void PrintEmpInfo()
+    {
+        base.PrintEmpInfo();
+        Console.WriteLine("The Emp From Part Time");
+    }
 }
-
 
 class Program
 {
     static void Main()
     {
-        FullTimeEmployee emp1 = new FullTimeEmployee();
-        emp1.FirstName = "ahmed";
-        emp1.LastName = "mohamady";
-        emp1.Address = "asdasdasda";
-        emp1.theSalary = 100.0F;
-        emp1.PrintEmpInfo();
+        PartTimeEmployee PREP = new PartTimeEmployee();
+        PREP.FirstName = "Ahmed";
+        PREP.LastName = "Mohamady";
+        PREP.PrintEmpInfo();
 
+        Console.WriteLine("=============================");
 
-        PartTimeEmployee emp2 = new PartTimeEmployee();
-     
-
-
+        //((Employee)PREP).PrintEmpInfo();
+        Employee PREP2 = new PartTimeEmployee();
+        PREP2.FirstName = "Mohamed";
+        PREP2.LastName = "Salah";
+        PREP2.PrintEmpInfo();
 
         Console.ReadKey(false);
  
