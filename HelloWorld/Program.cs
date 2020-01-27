357a7f3da8cfe2d1d45e3d2894cbf2767a798403
al Mohamady <al.Mohamady47@gmail.com>
2020-01-27 16:25:08 +0200

    Multicast Delegates in C#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,55 +1,48 @@
 ﻿using System;
 
-class Employee
-{
-    public int ID { get; set; }
-    public String Name { get; set; }
-    public Double Salary { get; set; }
-    public int Experience { get; set; }
-
-    public static void UpdateSalary(Employee[] emps , needUpdated NeedUpdated)
-    {
-        foreach (Employee emp in emps)
-        {
-            if (NeedUpdated(emp))
-            {
-                Console.WriteLine(" The Employee {0} is Updated ", emp.Name);
-            }
-        }
-    }
-}
-
-delegate bool needUpdated(Employee emp);
+public delegate int MyDelegate(out int x);
 
 class Program
 {
     static void Main()
     {
-        Employee[] employees = new Employee[4];
-        employees[0] = new Employee() { ID = 1, Name = "Ahmed", Experience = 6, Salary = 50000 };
-        employees[1] = new Employee() { ID = 2, Name = "Mohmaed", Experience = 2, Salary = 20000 };
-        employees[2] = new Employee() { ID = 3, Name = "Salah", Experience = 4, Salary = 10000 };
-        employees[3] = new Employee() { ID = 4, Name = "Ali", Experience = 5, Salary = 30000 };
+        MyDelegate myDelegate = new MyDelegate(myMethod);
 
-        needUpdated nu = new needUpdated(isNeedUpdate);
+        myDelegate += myMethod2;
+        myDelegate += myMethod3;
+        myDelegate -= myMethod2;
 
-        Employee.UpdateSalary(employees, nu);
-        
+        int x = 0; 
+        Console.WriteLine(myDelegate(out x));
+        Console.WriteLine(x);
+
+
+
         Console.ReadKey(true);
     }
 
-
-    public static bool isNeedUpdate(Employee emp)
+    public static int myMethod(out int x)
     {
-        if (emp.Experience >= 4)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        x = 1;
+        Console.WriteLine("Hello wrold {0}" , x);
+        return 1;
+    }
+
+    public static int myMethod2(out int x)
+    {
+        x = 2;
+        Console.WriteLine("Hello wrold {0}", x);
+        return 2; 
+    }
+
+    public static int myMethod3(out int x)
+    {
+        x = 3;
+        Console.WriteLine("Hello wrold {0}", x);
+        return 3;
     }
 }
+
+
 
 
