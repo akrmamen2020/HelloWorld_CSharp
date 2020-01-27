168327b62a55e839f55ddf7f6b5ac90caecf72fe
al Mohamady <al.Mohamady47@gmail.com>
2020-01-27 16:00:26 +0200

    Delegates in c#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,43 +1,54 @@
 ﻿using System;
 
-class myCalss
+class Employee
 {
     public int ID { get; set; }
+    public String Name { get; set; }
+    public Double Salary { get; set; }
+    public int Experience { get; set; }
 
-
-    private string name; 
-    public string Name {
-        get {
-            return name;
-        }  set {
-            name = value; 
-        } }
-
-    private int[] arr; 
-
-    public int[] Arr
+    public static void UpdateSalary(Employee[] emps , needUpdated NeedUpdated)
     {
-        get { return arr; }
-        set { arr = value; }
+        foreach (Employee emp in emps)
+        {
+            if (NeedUpdated(emp))
+            {
+                Console.WriteLine(" The Employee {0} is Updated ", emp.Name);
+            }
+        }
     }
-
-    public int[] arr2 { get; set; }
 }
 
+delegate bool needUpdated(Employee emp);
 
 class Program
 {
     static void Main()
     {
-        myCalss cls = new myCalss() { ID = 1, Name = "Ahmed"};
-        myCalss cls2 = new myCalss() { ID = 2 };
+        Employee[] employees = new Employee[4];
+        employees[0] = new Employee() { ID = 1, Name = "Ahmed", Experience = 6, Salary = 50000 };
+        employees[1] = new Employee() { ID = 2, Name = "Mohmaed", Experience = 2, Salary = 20000 };
+        employees[2] = new Employee() { ID = 3, Name = "Salah", Experience = 4, Salary = 10000 };
+        employees[3] = new Employee() { ID = 4, Name = "Ali", Experience = 5, Salary = 30000 };
 
-        //cls.ID = 1;
-        //cls.Name = "Ahmed";
+        needUpdated nu = new needUpdated(isNeedUpdate);
 
-        Console.WriteLine(cls.ID + "  " + cls.Name);
-
+        Employee.UpdateSalary(employees, nu);
+        
         Console.ReadKey(true);
+    }
+
+
+    public static bool isNeedUpdate(Employee emp)
+    {
+        if (emp.Experience >= 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
 
