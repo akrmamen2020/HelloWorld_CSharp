d4820ef5a7a80718b524495751a2f0b65a45b441
al Mohamady <al.Mohamady47@gmail.com>
2020-01-30 18:58:34 +0200

    Reflection in C#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,34 +1,81 @@
 ﻿using System;
+using System.Reflection;
 
 
-class Program
+namespace theSystem
 {
-    static void Main()
+    class Customer 
     {
-        Calculator.add(1, 2);
+        public int ID { get; set; }
+        public String Name { get; set; }
 
-
-        Console.ReadKey(true);
-    }
-}
-
-class Calculator
-{
-    [Obsolete("This Method is old use the new one", true)]
-    public static int add(int n1, int n2)
-    {
-        return n1 + n2;
-    }
-
-    public static int add(int n1, int n2 , bool x, params int[] numbers)
-    {
-        int sum = n1 + n2;
-        foreach (int i in numbers)
+        public Customer()
         {
-            sum += i;
+            ID = 0;
+            Name = "";
         }
-        return sum;
+
+        public Customer(int _ID, string _Name)
+        {
+            ID = _ID;
+            Name = _Name;
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine("ID IS {0}", ID);
+        }
+
+        public string PrintName()
+        {
+            Console.WriteLine("Name is {0}", Name);
+            return Name;
+        }
     }
+
+    class Program
+    {
+        static void Main()
+        {
+            Type t = Type.GetType("theSystem.Customer");
+
+            t = typeof(Customer);
+
+            Customer c = new Customer();
+            t = c.GetType();
+
+
+            Console.WriteLine( t.Name  + " /// " +   t.FullName);
+            
+            Console.WriteLine("=========================================");
+            Console.WriteLine("The Properties");
+            PropertyInfo[] propertyInfos = t.GetProperties();
+
+            foreach (PropertyInfo pi in propertyInfos)
+            {
+                Console.WriteLine(pi.PropertyType.Name + "  " + pi.Name);
+            }
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("The Methodes");
+            MethodInfo[] methodInfos = t.GetMethods();
+
+            foreach (MethodInfo mi in methodInfos)
+            {
+                Console.WriteLine(mi.ReturnType.Name + "  " + mi.Name);
+            }
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("The Cons");
+            ConstructorInfo[] consInfos = t.GetConstructors();
+
+            foreach (ConstructorInfo ci in consInfos)
+            {
+                Console.WriteLine(ci.ToString());
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+
 }
-
-
