f50c2791b1777f2cac5d365b85ed2582fc12d49a
al Mohamady <al.Mohamady47@gmail.com>
2020-02-18 12:33:10 +0200

    Generics in C#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,21 +7,50 @@ namespace theSystem
     {
         static void Main()
         {
-            String sql = " Select * From Table ";
-            sql += " where Col1 = 'asdsa' ";
-            sql += " and Col2 = 10000  ";
 
-            //Console.WriteLine(sql);
+            //Console.WriteLine(Calculator.addNumbers("49", 9)); 
 
-            StringBuilder sb = new StringBuilder("Select * From Table");
-            sb.Append(" where Col1 = 'asdsa' ");
-            sb.Append(" and Col2 = 10000  ");
+            MyNumbers m1 = new MyNumbers() { myNumber = 10 };
+            MyNumbers m2 = new MyNumbers() { myNumber = 10 };
+
+
+            Console.WriteLine(Calculator<MyNumbers>.equals(m1, m2));
+
+            Console.WriteLine(Calculator<int>.equals(10, 20));
+
+            Console.WriteLine(Calculator<string>.equals("10", "10"));
+
+            Console.ReadKey(true); 
+        }
+    }
+
+
+    public class Calculator<T>
+    {
+        public static int addNumbers(object num1, object num2)
+        { 
+            return Convert.ToInt16(num1) + Convert.ToInt16(num2);
 
             
 
-            Console.WriteLine(sb +  sql); 
+        }
 
-            Console.ReadKey(true); 
+        public static bool equals(T obj1, T obj2)
+        {
+            return obj1.Equals(obj2);
+        }
+
+    }
+
+
+    public class MyNumbers
+    {
+
+        public int myNumber { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.myNumber == ((MyNumbers)obj).myNumber;
         }
     }
 }
