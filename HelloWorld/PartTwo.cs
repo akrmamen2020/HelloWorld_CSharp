bc2431bd8d82791f52769ceae55d60a6db1b2930
al Mohamady <al.Mohamady47@gmail.com>
2020-03-02 13:19:23 +0200

    Indexers in c#

--- HelloWorld/PartTwo.cs
+++ HelloWorld/PartTwo.cs
@@ -15,17 +15,6 @@ namespace HelloWorld
     public  partial class Customer
     {
 
-        public void sayHi()
-        {
-            string name = _firstName + " " + _lastName;
-            theMethodP();
-            Console.WriteLine("Hi " + getFullName()); 
-        }
-
-        partial void theMethodP()
-        {
-            Console.WriteLine("Hi P");
-        }
     }
 
 
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,54 +1,85 @@
 ﻿using System;
-using System.Text; 
+using System.Text;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HelloWorld
 {
 
-    public class Father
-    { 
-    
+    class Emplyee
+    {
+        public int EmplyeeId { get; set; }
+        public string Name { get; set;  }
+        public string Gender { get; set; }
     }
 
-
-    public  partial  class Customer : Father
+    class Company
     {
+        private  List<Emplyee> emplyees;
 
-        private string _firstName;
-        private string _lastName;
-
-        partial void theMethodP(); 
-
-
-        public string LastName
-        { 
-            get { return _lastName; }
-            set { _lastName = value; }
-        }
-
-        public string FirstName
+        public Company()
         {
-            get { return _firstName; }
-            set { _firstName = value; }
+            emplyees = new List<Emplyee>();
+
+            emplyees.Add(new Emplyee() { EmplyeeId = 1, Name = "Ahmed", Gender = "Male" });
+            emplyees.Add(new Emplyee() { EmplyeeId = 2, Name = "Mohmed", Gender = "Male" });
+            emplyees.Add(new Emplyee() { EmplyeeId = 3, Name = "Salah", Gender = "Male" });
+            emplyees.Add(new Emplyee() { EmplyeeId = 4, Name = "Fatema", Gender = "Female" });
+            emplyees.Add(new Emplyee() { EmplyeeId = 5, Name = "Gada", Gender = "Female" });
         }
 
-        public string getFullName()
-        { 
-          return FirstName + " " + LastName;
+
+        public string this[int EmplyeeID]
+        {
+            get
+            {
+                string theName = "";
+                foreach (Emplyee e in emplyees)
+                {
+                    if (e.EmplyeeId == EmplyeeID)
+                    {
+                        theName = e.Name;
+                    }
+                }
+                return theName;
+            }
+            set
+            {
+                emplyees.FirstOrDefault(emp => emp.EmplyeeId == EmplyeeID).Name = value; 
+            }
         }
     }
 
 
     class Program
     {
+        
+
+
         static void Main()
         {
-            Customer c = new Customer();
-            c.sayHi();
-            c.newMethod();
 
+            Company c = new Company();
 
+            //c[1] = "Hissen";
+
+            Console.WriteLine(c[1]);  
+
+            //DataTable dt = new DataTable("myTable");
+            //dt.Columns.Add("Name");
+            //dt.Columns.Add("age");
+
+            //DataRow r = dt.NewRow();
+
+            //r[0] = "Ahmed";
+            //r["age"] = 35;
+
+            //Console.WriteLine(r[1]);
 
             Console.ReadKey(true); 
         }
+
+
     }
 }
